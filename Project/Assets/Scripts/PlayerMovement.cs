782db003830cc35b3b1ced78c0e1015b49deb147
Jelena Delic <jelenadelic10@gmail.com>
2023-06-06 23:16:19 +0200

    Onemoguceno skakanje ako nije na zemlji i popravljanje trcanja

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator anim;
+    private BoxCollider2D coll;
     private float dirX;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private LayerMask jumpableGround;
 
     private enum MoevementState { nm,rn,jumping,falling};
     
@@ -23,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -31,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -69,5 +72,10 @@ public class PlayerMovement : MonoBehaviour
 
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
